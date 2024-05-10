4037993a9d97a27198858ff69a90806ce862c7c5
AgaKab <agata,kabot@vp.pl>
2024-05-10 12:06:52 +0200

    empty class

---
+++ converter.py
@@ -0,0 +1,19 @@
+class TemperatureConverter:
+    
+    def celsius_to_fahrenheit(self, c):
+        pass
+    
+    def fahrenheit_to_celsius(self):
+        pass
+        
+    def celsius_to_kelvin(self):
+        pass
+        
+    def kelvin_to_celsius(self):
+        pass
+        
+    def fahrenheit_to_kelvin(self):
+        pass
+        
+    def kelvin_to_fahrenheit(self):
+        pass    
---
+++ test_converter.py
@@ -0,0 +1,10 @@
+import unittest
+from converter import TemperatureConverter
+
+class TestTemperatureConverter(unittest.TestCase):
+    def setUp(self):
+        self.converter = TemperatureConverter()
+
+if __name__=="__main":
+    unittest.main()
+            
