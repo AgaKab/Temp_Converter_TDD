7efc2f266d87097752a2c044087e0f0a3178d61c
AgaKab <agata,kabot@vp.pl>
2024-05-10 13:25:40 +0200

    test completed

--- converter.py
+++ converter.py
@@ -1,19 +1,26 @@
 class TemperatureConverter:
     
     def celsius_to_fahrenheit(self, c):
-        pass
+        f = (2 * c) + 32
+        return f
     
-    def fahrenheit_to_celsius(self):
-        pass
+    def fahrenheit_to_celsius(self, f):
+        c = (f-32)/2
+        return c
         
-    def celsius_to_kelvin(self):
-        pass
+    def celsius_to_kelvin(self, c):
+        k = c + 273.15
+        return k
         
-    def kelvin_to_celsius(self):
-        pass
+    def kelvin_to_celsius(self, k):
+        c = k-273.15
+        return c
+           
+    def fahrenheit_to_kelvin(self, f):
+        k = ((f-32)/1.8)+273.15
+        return round (k, 2)
         
-    def fahrenheit_to_kelvin(self):
-        pass
-        
-    def kelvin_to_fahrenheit(self):
-        pass    
+    def kelvin_to_fahrenheit(self, k):
+        f = ((k - 273.15) * 1.8)+32
+        return round (f, 2)
+            
--- test_converter.py
+++ test_converter.py
@@ -4,7 +4,25 @@ from converter import TemperatureConverter
 class TestTemperatureConverter(unittest.TestCase):
     def setUp(self):
         self.converter = TemperatureConverter()
+        
+    def test_celsius_to_fahrenheit(self):
+        self.assertEqual(66, self.converter.celsius_to_fahrenheit(17))
+    
+    def test_fahrenheit_to_celsius(self):
+        self.assertEqual(32, self.converter.fahrenheit_to_celsius(96))
+        
+    def test_celsius_to_kelvin(self):
+        self.assertEqual(290.15 ,self.converter.celsius_to_kelvin(17))
+        
+    def test_kelvin_to_celsius(self):
+        self.assertEqual(17, self.converter.kelvin_to_celsius(290.15))
+        
+    def test_fahrenheit_to_kelvin(self):
+        self.assertEqual(305.37, self.converter.fahrenheit_to_kelvin(90))
+        
+    def test_kelvin_to_fahrenheit(self):
+       self.assertEqual(83.93, self.converter.kelvin_to_fahrenheit(302))
 
-if __name__=="__main":
+if __name__=="__main__":
     unittest.main()
             
